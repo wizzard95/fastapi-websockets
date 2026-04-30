973e8dd486dba572b509963b8c02f2849d0b05a9
wizzard95 <damian110995a@gmail.com>
2026-04-30 18:30:21 -0400

    gestionar excepciones, desconexion y conexiones simples

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 # 1.- importar fastapi, apirouter y request 
-from fastapi import FastAPI, APIRouter, Request, WebSocket
+from fastapi import FastAPI, APIRouter, Request, WebSocket, WebSocketDisconnect
 
 # 2.- importar el template del formulario para conectarse al ws
 from fastapi.templating import Jinja2Templates
@@ -24,8 +24,24 @@ def form(request: Request):
 # * esperamos la conexion al wss
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept() # ! aceptamos la conexion
-    while True: 
+    try:
+# 10.- gestionar excepciones, desconectar y conexiones simples
+        while True: 
+          print('connection open')
 # * esto se ejectura en bucle hasta que se reciba una respuesta del usuario
-        data = await websocket.receive_text()
+          data = await websocket.receive_text()
         # * enviamos la data de vuelta
-        await websocket.send_text(f"Message text was: {data}")
+          await websocket.send_text(f"Message text was: {data}")
+
+    except WebSocketDisconnect:
+        # * EVENTO: AL DESCONECTARSE (CIERRE LIMPIO)
+        print("El cliente cerro la conexion")
+    
+    except Exception as e:
+        # * CAPTURA OTROS ERRORES INESPERADOS
+        print(f"Error Inesperado: {e}")
+    
+    finally:
+        # * LOGICA FINAL
+        # * este bloque se ejecuta siempre, ideal para limpieza de recursos
+        print("Limpieza de conexion finalizada")
